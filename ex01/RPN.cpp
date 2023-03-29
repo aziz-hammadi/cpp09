3cbe4a1c09924e6257d4052e42de0501cc6ab8dd
aziz-hammadi <a.hammadi.91@gmail.com>
2023-03-29 17:12:32 +0200

    ex01

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
-#include <string>
-
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
-        std::cerr << "les arguments ne sont pas suffisament\n";
-    }
-    else
-        std::cout << "ca marche" << std::endl;
-}
+//les fonctions
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,12 +1,75 @@
+#include "RPN.hpp"
+#include <stack>
 #include <iostream>
-#include <string>
+
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        std::cerr << "les arguments ne sont pas suffisament\n";
+        std::cerr << "les arguments ne sont pas suffisament\n"<< std::endl;
+        std::cerr << "Error" << std::endl;
     }
     else
-        std::cout << "ca marche" << std::endl;
+    {
+        std::string s = argv[1];
+        std::cout << "ca marche : " << s << std::endl;
+    }
+     std::string s = argv[1];
+
+    int i;
+    i = 0;
+    std::stack<int> pile;
+    while (s[i])
+    {
+        if (isdigit(s[i]))
+        {
+            pile.push(s[i] - '0');
+            std::cout << "chiffre" << std::endl;
+        }
+        else
+        {
+            if (s[i] == '+')
+                std::cout << "+" << std::endl;
+            else if (s[i] == '-')
+                std::cout << "-" << std::endl;
+            else if (s[i] == '/')
+                std::cout << "/" << std::endl;
+            else if (s[i] == '*')
+                std::cout << "*" << std::endl;
+            else
+            {
+                std::cout << "Error\n" << std::endl;
+                return (0);
+            }
+        }
+        i++;
+    }
+    if (!pile.empty())
+    {
+        std::cout << "ca marche : " << pile.top() << std::endl;
+        pile.pop();
+        std::cout << "ca marche : " << pile.top() << std::endl;
+        pile.pop();
+        std::cout << "ca marche : " << pile.top() << std::endl;
+    }
+    /*
+    int i;
+    i = 0;
+    std::stack<int> p = s; 
+
+    while (argv[1][i])
+    {
+        std::stack<int> p;
+        c >> i; 
+    }
+    
+    else
+    {
+        std::string s = argv[1];
+        std::cout << "ca marche : " << s << std::endl;
+    }
+    else
+        std::cout << "marche pas" << std::endl;*/
+    return (0);
 }
