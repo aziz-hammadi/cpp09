924326c6c6bd95b73fe3b3ab02ff5fe45ad7fa24
aziz-hammadi <a.hammadi.91@gmail.com>
2023-03-30 15:28:48 +0200

    ex01

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -1,5 +1,13 @@
 #ifndef RPN_HPP
 #define RPN_HPP
+#include <stack>
+#include <iostream>
 //nom des prototypes
 
+void    ft_addition(int a, int b, std::stack<int> *pile);
+void    ft_reverse(std::stack<int>& pile);
+void    insert_at_bottom(std::stack<int>& pile, int x);
+void    ft_soustraction(int a, int b, std::stack<int> *pile);
+void    ft_multiplication(int a, int b, std::stack<int> *pile);
+void    ft_division(int a, int b, std::stack<int> *pile);
 #endif
